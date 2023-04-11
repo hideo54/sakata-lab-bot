f458685ea670438ac368b91533daf7687044074e
Hideo Yasumoto <hideo54@hideo54.com>
2023-04-11 22:07:44 +0900

    Remove unused hourly job with faculty news bot

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,11 @@
 import * as functions from 'firebase-functions';
-import admin from 'firebase-admin'; // Default import required
-import { getFirestore } from 'firebase-admin/firestore';
 import { App, ExpressReceiver } from '@slack/bolt';
 import dotenv from 'dotenv';
 dotenv.config();
-import facultyNews from './facultyNews';
 import notifier from './notifier';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const randomChannel = process.env.SLACK_RANDOM_CHANNEL!;
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const facultyNewsChannel = process.env.SLACK_FACULTY_NEWS_CHANNEL!;
 
 const receiver = new ExpressReceiver({
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -24,16 +19,6 @@ const slackApp = new App({
     signingSecret: process.env.SLACK_SIGNING_SECRET!,
     receiver,
 });
-
-export const sakataLabSlackHourlyJob = functions
-    .region('asia-northeast1')
-    .pubsub.schedule('0 * * * *')
-    .timeZone('Asia/Tokyo')
-    .onRun(async () => {
-        admin.initializeApp();
-        const firestoreDb = getFirestore();
-        await facultyNews({ slackApp, firestoreDb, channel: facultyNewsChannel });
-    });
 
 export const sakataLabSlackEventsReceiver = functions
     .region('asia-northeast1')
