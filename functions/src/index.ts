a7054c6cd5a5a7240c58d678ea95cd5938ebf187
Hideo Yasumoto <hideo54@hideo54.com>
2026-06-13 17:30:14 +0900

    oxfmt

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -87,7 +87,7 @@ server.get("/mackerel/graphs/:hostId/:metricLabel", async (req, res) => {
         responseType: "arraybuffer",
       },
     );
-    if (typeof mackerelRes.headers["content-type"] === 'string') {
+    if (typeof mackerelRes.headers["content-type"] === "string") {
       res.setHeader("Content-Type", mackerelRes.headers["content-type"]);
     }
     res.send(mackerelRes.data);
